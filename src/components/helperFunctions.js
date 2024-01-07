21cd101314663e189e56a44bd251efcb04ce91e3
ErkiD <erkidorbek@gmail.com>
2024-01-07 19:38:23 +0200

    Rewrite birthday helper functions

--- src/components/helperFunctions.js
+++ src/components/helperFunctions.js
@@ -1,49 +1,44 @@
-export const getBirthDateFromIdCode = (idCode) => {
+const getBirthdateYearFromIdCode = (idCode) => {
   const code = idCode.toString();
   const yearCode = code.substring(0, 1);
-  let year;
+  const day = code.substring(5, 7);
+  const month = code.substring(3, 5);
+  const yearEnd = code.substring(1, 3);
+  let yearStart;
+
   switch (yearCode) {
     case '1':
     case '2':
-      year = '18';
+      yearStart = '18';
       break;
     case '3':
     case '4':
-      year = '19';
+      yearStart = '19';
       break;
     case '5':
     case '6':
-      year = '20';
+      yearStart = '20';
+      break;
+    case '7':
+    case '8':
+      yearStart = '21';
       break;
     default:
-      year = '19';
+      yearStart = '19';
       break;
   }
 
-  return new Date(year + code.substring(1, 3), code.substring(3, 5), code.substring(5, 7), 0, 0, 0, 0);
+  return { yearStart, yearEnd, day, month };
+};
+
+export const getBirthDateFromIdCode = (idCode) => {
+  const { yearStart, yearEnd, day, month } = getBirthdateYearFromIdCode(idCode);
+
+  return new Date(yearStart + yearEnd, month, day, 0, 0, 0, 0);
 };
 
 export const formatIdCode = (idCode) => {
-  const code = idCode.toString();
-  const yearCode = code.substring(0, 1);
-  let year;
-  switch (yearCode) {
-    case '1':
-    case '2':
-      year = '18';
-      break;
-    case '3':
-    case '4':
-      year = '19';
-      break;
-    case '5':
-    case '6':
-      year = '20';
-      break;
-    default:
-      year = '19';
-      break;
-  }
+  const { yearStart, yearEnd, day, month } = getBirthdateYearFromIdCode(idCode);
 
-  return code.substring(5, 7) + '.' + code.substring(3, 5) + '.' + year + code.substring(1, 3);
+  return day + '.' + month + '.' + yearStart + yearEnd;
 };
